cea244a2b2ff3f0cf02168a702e8e136cddff288
Chasky26 <david_C91@outlook.com>
2024-06-25 13:07:45 -0300

    Catalogo añadido

--- app.py
+++ app.py
@@ -1,3 +1,35 @@
+import mysql.connector
+
+class Catalogo:
+
+  def __init__(self, host, user, password, database):
+
+    self.conn = mysql.connector.connect(
+      host=host,
+      #port=port,
+      user=user,
+      password=password,
+      database=database     
+    )
+
+    self.cursor = self.conn.cursor(dictionary=True)
+    self.cursor.execute('''CREATE TABLE IF NOT EXISTS productos (
+      codigo INT AUTO_INCREMENT PRIMARY KEY,
+      descripcion VARCHAR(255) NOT NULL,
+      cantidad INT NOT NULL,
+      precio DECIMAL(10, 2) NOT NULL,
+      imagen_url VARCHAR(255),
+      proveedor INT(4)''')
+    self.conn.commit()
+
+  def agregar_producto(self, codigo, description, cantidad, precio, imagen, proveedor):
+    sql = "INSERT productos (descripcion, cantidad,precio, imagen, proveedor) VALUES (%s, %s, %s, %s, %s)"
+    valores = (description, cantidad, precio, imagen, proveedor)
+
+    self.cursor.execute(sql, valores)
+    self.conn.commit()
+    return self.cursor.lastrowid
+
 
 #lista para productos
 productos = [] 
@@ -72,33 +104,28 @@ agregar_producto(3, "pad optico", 300, 155.50, "imagen.jpg","Vaultec")
 agregar_producto(4, "impresoa", 400, 15550.50, "imagen.jpg","Checa")
 agregar_producto(5, "Mando", 250, 15500, "imagen.jpg","Peru")
 
-listar_productos()
+# listar_productos()
 
-#consultar Producto
-codigo_pro = int(input("Ingrese el codigo del producto: "))
-producto = consultar_producto(codigo_pro)
+# #consultar Producto
+# codigo_pro = int(input("Ingrese el codigo del producto: "))
+# producto = consultar_producto(codigo_pro)
 
-if producto:
-  print(f"Producto encontraso {producto['codigo']} - {producto['descripcion']}")
-else:
-  print(f"Producto con codigo {codigo_pro} no encontrado")
+# if producto:
+#   print(f"Producto encontraso {producto['codigo']} - {producto['descripcion']}")
+# else:
+#   print(f"Producto con codigo {codigo_pro} no encontrado")
 
-modificar_producto(5, "Mando peruano", 99, 8522.20,"causa.jpg","Peru Tic")
+# modificar_producto(5, "Mando peruano", 99, 8522.20,"causa.jpg","Peru Tic")
 
-print("#"*50)
-#listar productos de nuevo
-listar_productos()
+# print("#"*50)
+# #listar productos de nuevo
+# listar_productos()
 
-#eliminar producto
-eliminar_producto(5)
+# #eliminar producto
+# eliminar_producto(5)
 
-print("*"*50)
-#listamos productos de nuevo
-listar_productos()
+# print("*"*50)
+# #listamos productos de nuevo
+# listar_productos()
 
-
-
-
-
-
-
+catalogo = Catalogo(host= 'localhost', user='root', password='root', database='miapp')
